82c88e6dd1ac3405afc19717177c7e6cd179bee0
Diego Arturo Enriquez Mercado <93006286+Digiko2003@users.noreply.github.com>
2023-04-24 16:20:33 -0600

    24-04

---
+++ src/MuertesEverest/Personas_M.java
@@ -0,0 +1,11 @@
+package MuertesEverest;
+
+public class Personas_M {
+
+    public String Nombre;
+    public String Fecha;
+    public double Edad;
+    public String Expedicion;
+    public String CausaDeMuerte;
+
+}
